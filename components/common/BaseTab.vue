885c715c5d131646ae1718c40f0bb04cacb1a150
Luis Velásquez <luvejo.dev@gmail.com>
2023-11-01 07:29:53 -0500

    feat: implement tab panel logic using Headless UI

---
+++ components/common/BaseTab.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Tab } from '@headlessui/vue'
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <Tab
+    class="no-outline-on-click relative flex w-14 flex-col items-center gap-1 px-1 pb-2 pt-2"
+    v-slot="{ selected }"
+  >
+    <span :class="{ 'opacity-50': !selected }">
+      <slot />
+    </span>
+
+    <p :class="{ 'opacity-60': !selected }">{{ label }}</p>
+
+    <hr
+      v-if="selected"
+      class="absolute bottom-0 left-0 h-1 w-full border-none bg-orange-500"
+    />
+  </Tab>
+</template>
--- components/widgets/ActionPanel.vue
+++ components/widgets/ActionPanel.vue
@@ -1,60 +1,65 @@
+<script setup lang="ts">
+import { TabGroup, TabList, TabPanels, TabPanel } from '@headlessui/vue'
+</script>
+
 <template>
-  <section class="px-10">
-    <!-- Tabs -->
-    <div class="relative flex justify-between text-xs font-semibold">
-      <button class="relative flex flex-col items-center gap-1 px-1 pb-2">
+  <TabGroup as="div" class="px-10">
+    <TabList class="relative -mt-2 flex justify-between text-xs font-semibold">
+      <BaseTab label="Prompts">
         <SparkIcon />
-        <p> Prompts </p>
+      </BaseTab>
 
-        <hr
-          class="absolute bottom-0 left-0 h-1 w-full border-none bg-orange-500"
-        />
-      </button>
+      <BaseTab label="Chat">
+        <DialogIcon />
+      </BaseTab>
 
-      <button class="relative flex flex-col items-center gap-1 px-1 pb-2">
-        <DialogIcon class="opacity-50" />
-        <p class="opacity-60"> Chat </p>
-      </button>
-
-      <button class="relative flex flex-col items-center gap-1 px-1 pb-2">
-        <HistoryIcon class="opacity-50" />
-        <p class="opacity-60"> History </p>
-      </button>
+      <BaseTab label="History">
+        <HistoryIcon />
+      </BaseTab>
 
       <hr
         class="absolute bottom-0 left-0 h-1 w-full border-none bg-gray-900/5"
       />
-    </div>
+    </TabList>
 
-    <!-- Panel Content -->
-    <div class="mt-5">
-      <p class="text-xs font-semibold"> Perspective </p>
+    <TabPanels>
+      <TabPanel>
+        <div class="mt-5">
+          <p class="text-xs font-semibold"> Perspective </p>
 
-      <ul class="mt-2 flex flex-col gap-y-3">
-        <PromptButton secondary-label="Insert" main-label="Rhetorical Figure" />
-        <PromptButton secondary-label="Change" main-label="Point of View" />
-        <PromptButton secondary-label="Change" main-label="Tense" />
-      </ul>
-    </div>
+          <ul class="mt-2 flex flex-col gap-y-3">
+            <PromptButton
+              secondary-label="Insert"
+              main-label="Rhetorical Figure"
+            />
+            <PromptButton secondary-label="Change" main-label="Point of View" />
+            <PromptButton secondary-label="Change" main-label="Tense" />
+          </ul>
+        </div>
 
-    <div class="mt-5">
-      <p class="text-xs font-semibold"> Editing </p>
+        <div class="mt-5">
+          <p class="text-xs font-semibold"> Editing </p>
 
-      <ul class="mt-2 flex flex-col gap-y-3">
-        <PromptButton secondary-label="Make it" main-label="Weird" />
-        <PromptButton
-          secondary-label="Suggest"
-          main-label="Ironic Turning Points"
-        />
-      </ul>
-    </div>
+          <ul class="mt-2 flex flex-col gap-y-3">
+            <PromptButton secondary-label="Make it" main-label="Weird" />
+            <PromptButton
+              secondary-label="Suggest"
+              main-label="Ironic Turning Points"
+            />
+          </ul>
+        </div>
 
-    <div class="mt-5">
-      <p class="text-xs font-semibold"> Other </p>
+        <div class="mt-5">
+          <p class="text-xs font-semibold"> Other </p>
 
-      <ul class="mt-2 flex flex-col gap-y-3">
-        <PromptButton secondary-label="Ctrl + Enter" main-label="Raw Query" />
-      </ul>
-    </div>
-  </section>
+          <ul class="mt-2 flex flex-col gap-y-3">
+            <PromptButton
+              secondary-label="Ctrl + Enter"
+              main-label="Raw Query"
+            />
+          </ul>
+        </div>
+      </TabPanel>
+    </TabPanels>
+  </TabGroup>
 </template>
