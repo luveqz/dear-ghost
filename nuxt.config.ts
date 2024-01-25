12e11138597f8dd74a3aeb190028d2245d00155b
Luis Velásquez <luvejo.dev@gmail.com>
2024-01-24 20:57:09 -0500

    refactor: move makeId() to the /lib/utils folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,4 @@
-function makeid(length: number) {
-  let result = ''
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  const charactersLength = characters.length
-  let counter = 0
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength))
-    counter += 1
-  }
-  return result
-}
+import { makeId } from './lib/utils/random'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -57,8 +46,8 @@ export default defineNuxtConfig({
   security: {
     basicAuth: {
       include: ['/'],
-      name: process.env.BASIC_AUTH_USER || makeid(60),
-      pass: process.env.BASIC_AUTH_PASS || makeid(60),
+      name: process.env.BASIC_AUTH_USER || makeId(60),
+      pass: process.env.BASIC_AUTH_PASS || makeId(60),
       enabled: process.env.NODE_ENV !== 'development',
     },
   },
