13f271aeff8fab4d412e772c0ca94b478c8dfd18
Luis Velásquez <luvejo.dev@gmail.com>
2024-03-08 22:28:47 -0500

    feat: define small and medium BaseInput variants

--- components/common/BaseInput.vue
+++ components/common/BaseInput.vue
@@ -4,6 +4,10 @@ defineProps({
     type: String,
     required: true,
   },
+  variant: {
+    type: String as PropType<'small' | 'medium'>,
+    default: 'small',
+  },
 })
 
 defineEmits(['update:model-value'])
@@ -12,7 +16,8 @@ defineEmits(['update:model-value'])
 <template>
   <input
     type="text"
-    class="border-blue-gray-200 h-10 rounded border bg-white p-2.5 px-2 text-sm font-medium placeholder:opacity-40"
+    class="h-10 rounded border border-blue-gray-200 bg-white p-2.5 font-medium placeholder:opacity-80"
+    :class="variant === 'small' ? 'px-2 text-sm' : 'px-4 text-base'"
     :value="modelValue"
     @input="
       $emit('update:model-value', ($event.target as HTMLInputElement).value)
