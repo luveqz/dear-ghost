c9619a37f2bf7bfc02f7d1c2d3fc3ab3e9eebe91
Luis Velásquez <luvejo.dev@gmail.com>
2024-07-12 12:06:09 -0500

    chore: remove no longer necessary constants, env vars, and adapters

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,6 @@
-/*
---------------------------------------------------
+/*--------------------------------------------------
   User Config
---------------------------------------------------
-*/
-
+--------------------------------------------------*/
 export const defaultConfig = {
   view: {
     fileTree: false,
@@ -25,68 +22,3 @@ export const defaultConfig = {
     },
   },
 }
-
-/*
---------------------------------------------------
-  LM Studio
---------------------------------------------------
-*/
-export const LM_STUDIO_API_BASE_URL = 'http://localhost:1234/v1'
-
-/*
---------------------------------------------------
-  LLaMA C++ / HuggingFace / Local
---------------------------------------------------
-*/
-export const MISTRAL_7B_FILENAME = 'mistral-7b-v0.1.Q4_K_M.gguf'
-export const LLAMACPP_API_PORT = '11432'
-export const LLAMACPP_API_BASE_URL = `http://localhost:${LLAMACPP_API_PORT}`
-
-/*
---------------------------------------------------
-  Ollama
---------------------------------------------------
-*/
-export const OLLAMA_API_BASE_URL = 'http://localhost:11434'
-
-/*
---------------------------------------------------
-  Clarifai
---------------------------------------------------
-*/
-export const CLARIFAI_API_BASE_URL = 'https://api.clarifai.com/v2/models/'
-
-/*
---------------------------------------------------
-  Google: Palm 2
---------------------------------------------------
-*/
-export const PALM_2_API_BASE_URL =
-  'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText'
-
-export const PALM_API_SAFETY_SETTINGS = [
-  {
-    category: 'HARM_CATEGORY_DEROGATORY',
-    threshold: 'BLOCK_NONE',
-  },
-  {
-    category: 'HARM_CATEGORY_TOXICITY',
-    threshold: 'BLOCK_NONE',
-  },
-  {
-    category: 'HARM_CATEGORY_VIOLENCE',
-    threshold: 'BLOCK_NONE',
-  },
-  {
-    category: 'HARM_CATEGORY_SEXUAL',
-    threshold: 'BLOCK_NONE',
-  },
-  {
-    category: 'HARM_CATEGORY_MEDICAL',
-    threshold: 'BLOCK_NONE',
-  },
-  {
-    category: 'HARM_CATEGORY_DANGEROUS',
-    threshold: 'BLOCK_NONE',
-  },
-]
