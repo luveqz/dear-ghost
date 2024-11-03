f71043d4099f3239f3fe784d9d39eb5447e33c13
Luis Velásquez <luvejo.dev@gmail.com>
2024-11-02 21:54:36 -0500

    perf: only inject analytics script to the home page

--- server/plugins/render.ts
+++ server/plugins/render.ts
@@ -1,7 +1,9 @@
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('render:html', (html) => {
-    html.head.push(
-      '<script defer src="/js/pa.js" data-api="/api/event" data-domain="dearghost.co"></script>',
-    )
+  nitroApp.hooks.hook('render:html', (html, ctx) => {
+    if (ctx.event.path === '/') {
+      html.head.push(
+        '<script defer src="/js/pa.js" data-api="/api/event" data-domain="dearghost.co"></script>',
+      )
+    }
   })
 })
